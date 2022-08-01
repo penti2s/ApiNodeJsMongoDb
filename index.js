a55963d07bfd31981cc38c221ddcaae6ce6016d1
Sergio Sepulveda <contacto@penti2s.com>
2022-08-01 12:27:21 -0400

    (feat): Creacion de Get, Put Patch y delete

--- index.js
+++ index.js
@@ -6,6 +6,22 @@ app.get('/', (req, res) => {
     res.status(200).send('Primer Get')
 })
 
+app.get('/:id', (req, res) => {
+    res.status(200).send('Get con parametro ' + req.params.id)
+})
+
+app.put('/:id', (req, res) => {
+    res.sendStatus(204)
+})
+
+app.patch('/:id', (req, res) => {
+    res.sendStatus(204)
+})
+
+app.delete('/:id', (req, res) => {
+    res.sendStatus(204)
+})
+
 app.listen(port, ()=> {
     console.log('Server inicializado')
 })
